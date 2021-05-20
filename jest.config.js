6e13697470a81fef4d519f118a8e9c9042fd27ca
Nil Késede <nilkesede@gmail.com>
2021-05-20 00:28:35 -0300

    ci: setup codeclimate

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   bail: true,
   collectCoverage: true,
+  coverageDirectory: 'tests/coverage',
   coveragePathIgnorePatterns: ['./node_modules'],
   setupFiles: ['./tests/_setup.mjs'],
   testEnvironment: 'node'
